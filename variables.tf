2b5f741806d49d2d0f921838925e31e2694148eb
Jeremy Collins <collins.jeremy@gmail.com>
2021-06-09 16:03:41 -0400

    Terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "filename" {
-  type    = string
-  default = "hello_local_file.txt"
+  type        = string
+  default     = "hello_local_file.txt"
   description = "It's a file name!"
 }
 variable "content" {
-  type    = string
-  default = "Hello World!"
+  type        = string
+  default     = "Hello World!"
   description = "It's some content!"
 }
 # Good comment
 variable "unused" {
-  type    = string
-  default = "I'm all alone in the world!"
+  type        = string
+  default     = "I'm all alone in the world!"
   description = "It's unused!"
 }
