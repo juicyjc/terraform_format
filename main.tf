f8284239b7007b1a2c46baae47f2757cbe7679ed
Jeremy Collins <collins.jeremy@gmail.com>
2021-06-09 11:00:09 -0400

    Test TFLint

--- main.tf
+++ main.tf
@@ -10,3 +10,7 @@ resource "local_file" "hello_local_file" {
   content  = var.content
   filename = var.filename
 }
+
+locals {
+  local_unused = "So sad and lonely :("
+}
--- var.tf
+++ var.tf
@@ -1,9 +1,12 @@
 variable "filename" {
+  type = "string"
   default = "hello_local_file.txt"
 }
 variable "content" {
+  type = "string"
   default = "Hello World!"
 }
+// Bad comment
 variable "unused" {
   default = "I'm all alone in the world!"
 }
