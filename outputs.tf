5f2299fcf3be7b95e98e696918abc537bdd7882c
jcollinsKR <jeremy.collins@kroger.com>
2021-06-09 14:32:43 -0400

    Terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -7,10 +7,10 @@ output "content" {
   description = "I'm content!"
 }
 output "output_no_description" {
-  value = "I have no description :("
+  value       = "I have no description :("
   description = "Oh yes I do!"
 }
 output "unused" {
-  value = var.unused
+  value       = var.unused
   description = "A previously unused comment"
 }
