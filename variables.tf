cec450471542e4953f0bc46b5a4ebe84b0e9b0e6
Jeremy Collins <collins.jeremy@gmail.com>
2021-06-09 16:02:16 -0400

    Add descriptions

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "filename" {
   type    = string
   default = "hello_local_file.txt"
+  description = "It's a file name!"
 }
 variable "content" {
   type    = string
   default = "Hello World!"
+  description = "It's some content!"
 }
 # Good comment
 variable "unused" {
   type    = string
   default = "I'm all alone in the world!"
+  description = "It's unused!"
 }
