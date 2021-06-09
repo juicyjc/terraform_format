0cbfcdd96800086fc107722d4fdcafcd750d56fe
jcollinsKR <jeremy.collins@kroger.com>
2021-06-09 15:06:03 -0400

    Changes for tflint

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
-# terraform {
-#   required_version = "> 0.7.0"
-#   required_providers {
-#     azuread = {
-#       source  = "hashicorp/azuread"
-#       version = ">= 0.13"
-#     }
-#   }
-# }
+terraform {
+  required_version = "> 0.7.0"
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 0.13"
+    }
+  }
+}
 resource "local_file" "hello_local_file" {
   content  = var.content
   filename = var.filename
+  version = ">= 0.1"
 }
