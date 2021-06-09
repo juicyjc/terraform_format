18e41c9984fdc83fbb9541d57c15db653f1849ab
jcollinsKR <jeremy.collins@kroger.com>
2021-06-09 14:05:43 -0400

    Format tf files

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "filename" {
-  value = var.filename
+  value       = var.filename
   description = "I'm a file name!"
 }
 output "content" {
-  value = var.content
+  value       = var.content
   description = "I'm content!"
 }
 output "output_no_description" {
-  value="I have no description :("
+  value = "I have no description :("
 }
